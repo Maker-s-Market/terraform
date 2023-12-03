7fdfa8799e9a42940c63be2ff623fa8381ad30d0
Mateus Almeida <mbcsalmeida@ua.pt>
2023-12-03 00:13:18 +0000

    variables in root module

--- main.tf
+++ main.tf
@@ -18,3 +18,73 @@ module "aws_database" {
 module "aws_vpc" {
     source = "./aws_vpc"
 } */
+
+variable "MYSQL_ROOT_PASSWORD" {
+  type        = string
+  default     = "MYSQL_ROOT_PASSWORD"
+}
+
+variable "MYSQL_DATABASE" {
+  type        = string
+  default     = "MYSQL_DATABASE"
+}
+
+variable "MYSQL_USER" {
+  type        = string
+  default     = "MYSQL_USER"
+}
+
+variable "MYSQL_PASSWORD" {
+  type        = string
+  default     = "MYSQL_PASSWORD"
+}
+
+variable "MYSQL_HOST" {
+  type        = string
+  default     = "MYSQL_HOST"
+}
+
+variable "AWS_ACCESS_KEY_ID"{
+  type        = string
+  default     = "AWS_ACCESS_KEY_ID"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  default     = "AWS_SECRET_ACCESS_KEY"
+}
+
+variable "BUCKET_NAME" {
+  type        = string
+  default     = "BUCKET_NAME"
+}
+
+variable "USER_POOL_ID" {
+  type        = string
+  default     = "USER_POOL_ID"
+}
+
+variable "COGNITO_USER_CLIENT_ID" {
+  type        = string
+  default     = "COGNITO_USER_CLIENT_ID"
+}
+
+variable "COGNITO_DOMAIN" {
+  type        = string
+  default     = "COGNITO_DOMAIN"
+}
+
+variable "AUTH_URL" {
+  type        = string
+  default     = "https://COGNITO_DOMAIN/oauth2/authorize"
+}
+
+variable "TOKEN_URL" {
+  type        = string
+  default     = "https://COGNITO_DOMAIN/oauth2/token"
+}
+
+variable "LOGOUT_URL" {
+  type        = string
+  default     = "https://COGNITO_DOMAIN/logout"
+}
