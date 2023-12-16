12eaecc3eb3d6ffcb3af06efe3432768761aadc8
Filipe Silveira <filipe.silveira@ua.pt>
2023-12-16 19:10:17 +0000

    Updated main variables

--- main.tf
+++ main.tf
@@ -18,6 +18,12 @@ module "aws_database" {
 module "aws_vpc" {
     source = "./aws_vpc"
 } */
+// Variables
+variable "aws_region" {
+  type        = string
+  description = "The region in which the resources will be created"
+  default     = "us-east-1"
+}
 
 variable "MYSQL_ROOT_PASSWORD" {
   type        = string
@@ -37,6 +43,11 @@ variable "MYSQL_USER" {
 variable "MYSQL_PASSWORD" {
   type        = string
   default     = "MYSQL_PASSWORD"
+}
+
+variable "MYSQL_SG" {
+  type        = string
+  default     = "MYSQL_SG"
 }
 
 variable "MYSQL_HOST" {
@@ -76,24 +87,67 @@ variable "COGNITO_DOMAIN" {
 
 variable "AUTH_URL" {
   type        = string
-  default     = "https://COGNITO_DOMAIN/oauth2/authorize"
+  default     = "AUTH_URL"
 }
 
 variable "TOKEN_URL" {
   type        = string
-  default     = "https://COGNITO_DOMAIN/oauth2/token"
+  default     = "TOKEN_URL"
 }
 
 variable "LOGOUT_URL" {
   type        = string
-  default     = "https://COGNITO_DOMAIN/logout"
+  default     = "LOGOUT_URL"
 }
 
-// Variables
-variable "aws_region" {
+variable "MAKERS_URL_API" {
   type        = string
-  description = "The region in which the resources will be created"
-  default     = "us-east-1"
+  default     = "MAKERS_URL_API"
+}
+
+variable "FRONTEND_SIGN_UP_IDP_LINK" {
+  type = string
+  default = "FRONTEND_SIGN_UP_IDP_LINK"
+}
+
+variable "STRIPE_KEY" {
+  type       = string
+  default    = "STRIPE_KEY"
+}
+
+variable "API_KEY_EMAIL"{
+  type       = string
+  default    = "API_KEY_EMAIL"
+}
+
+variable "VITE_STRIPE_KEY"{
+  type       = string
+  default    = "VITE_STRIPE_KEY"
+}
+
+variable "VITE_GOOGLE_AUTH_URL"{
+  type       = string
+  default    = "VITE_GOOGLE_AUTH_URL"
+}
+
+variable "SSL_CERTIFICATE_ARN" {
+  type        = string
+  default     = "SSL_CERTIFICATE_ARN"
+}
+
+variable "VPC_ID" {
+  type    = string
+  default = "VPC_ID"
+}
+
+variable "PUBLIC_SUBNET_1" {
+  type    = string
+  default = "PUBLIC_SUBNET_1"
+}
+
+variable "PUBLIC_SUBNET_2" {
+  type    = string
+  default = "PUBLIC_SUBNET_2"
 }
 
 // Providers
